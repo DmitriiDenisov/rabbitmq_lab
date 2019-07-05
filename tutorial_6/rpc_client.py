f54bf02b2b41d8d6b4d1854a5adc79e5e33977ff
Dmitry <dmitryhse@gmail.com>
2019-07-05 09:45:50 +0300

    refactoring tutorial 6

--- tutorial_6/rpc_client.py
+++ tutorial_6/rpc_client.py
@@ -2,24 +2,25 @@
 import pika
 import uuid
 
-class FibonacciRpcClient(object):
 
+class FibonacciRpcClient(object):
     def __init__(self):
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(host='localhost'))
 
         self.channel = self.connection.channel()
 
-        result = self.channel.queue_declare(queue='', exclusive=True)
+        # Create temporary queue for answer
+        result = self.channel.queue_declare(queue='', exclusive=True)  # this is queue of publisher!
         self.callback_queue = result.method.queue
 
-        self.channel.basic_consume(
+        self.channel.basic_consume(  # will listen to our temp queue in order to get answer
             queue=self.callback_queue,
             on_message_callback=self.on_response,
             auto_ack=True)
 
     def on_response(self, ch, method, props, body):
-        if self.corr_id == props.correlation_id:
+        if self.corr_id == props.correlation_id:  # waiting for response from consumer with correlation_id
             self.response = body
 
     def call(self, n):
@@ -27,19 +28,19 @@ class FibonacciRpcClient(object):
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(
             exchange='',
-            routing_key='rpc_queue',
+            routing_key='rpc_queue',  # this is queue of Consumer !
             properties=pika.BasicProperties(
-                reply_to=self.callback_queue,
+                reply_to=self.callback_queue,  # send name of queue to which send an answer
                 correlation_id=self.corr_id,
             ),
             body=str(n))
-        while self.response is None:
+        while self.response is None:  # endless loop for waiting for response
             self.connection.process_data_events()
-        return int(self.response)
+        return int(self.response)  # print the response from consumer
 
 
 fibonacci_rpc = FibonacciRpcClient()
 
-print(" [x] Requesting fib(30)")
-response = fibonacci_rpc.call(7)
-print(" [.] Got %r" % response)
+print(" [x] Requesting fib(7)")
+response = fibonacci_rpc.call(7)  # Get request to server
+print(" [.] Got %r" % response)  # Get answer from it
--- tutorial_6/rpc_server.py
+++ tutorial_6/rpc_server.py
@@ -24,14 +24,18 @@ def on_request(ch, method, props, body):
     print(" [.] fib(%s)" % n)
     response = fib(n)
 
+    # After we got message and find fib(n) we publish it to
     ch.basic_publish(exchange='',
-                     routing_key=props.reply_to,
-                     properties=pika.BasicProperties(correlation_id = \
-                                                         props.correlation_id),
-                     body=str(response))
+                     routing_key=props.reply_to,  # name of temp queue to send an answer
+                     properties=pika.BasicProperties(correlation_id= \
+                                                         props.correlation_id),  # correlation_id of message
+                     body=str(response)  # fib(n)
+                     )
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
+# We might want to run more than one server process.
+# In order to spread the load equally over multiple servers we need to set the prefetch_count setting.
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue='rpc_queue', on_message_callback=on_request)
 
